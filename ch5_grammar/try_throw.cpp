21088582ba990140d97fce2b82d625606f0c19be
luoshao23 <luoshao23@gmail.com>
2019-12-24 11:16:02 +0800

    mod ch5 try_throw

--- ch5_grammar/try_throw.cpp
+++ ch5_grammar/try_throw.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main()
 {
 	int n;
-	cin >> n;
-	try {
-		if (n > 100)
-			cout << "OK" << endl;
-		else
-			throw runtime_error("<Error 1: missing>");
-	} catch (runtime_error e) {
-		cout << e.what() << " occurred" << endl;
-		cout << "handle error..." << endl;
+	while (cin >> n ) 
+	{
+		try 
+		{
+			if (n > 100)
+				cout << "OK" << endl;
+			else
+				throw runtime_error("<Error 1: missing>");
+		} catch (runtime_error e) {
+			cout << e.what() << " occurred" << endl;
+			cout << "handle error..." << endl;
+		}
 	}
 	return 0;
 }
